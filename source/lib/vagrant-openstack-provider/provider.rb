133054b999951c35f7e3c63623b8f82827827505
rossi <f2brossi@gmail.com>
2014-11-06 22:23:02 +0100

    pass rake

--- source/lib/vagrant-openstack-provider/provider.rb
+++ source/lib/vagrant-openstack-provider/provider.rb
@@ -31,6 +31,7 @@ module VagrantPlugins
         # what it says. It puts the state in the `:machine_state_id`
         # key in the environment.
         env = @machine.action('read_state')
+
         state_id = env[:machine_state_id]
 
         # Get the short and long description
