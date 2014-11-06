91faaf060eb97d384e5459c9b7f3fd57f3c428ee
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2014-11-06 23:52:46 +0100

    Show image details if Glance is available

--- source/lib/vagrant-openstack-provider/command/image_list.rb
+++ source/lib/vagrant-openstack-provider/command/image_list.rb
@@ -12,8 +12,18 @@ module VagrantPlugins
         end
         def cmd(name, argv, env)
           fail Errors::NoArgRequiredForCommand, cmd: name unless argv.size == 0
-          images = env[:openstack_client].nova.get_all_images(env)
-          display_item_list(env, images)
+          rows = []
+          headers = %w(Id Name)
+          if env[:openstack_client].session.endpoints.key? :image
+            images = env[:openstack_client].glance.get_all_images(env)
+            images.each { |i| rows << [i.id, i.name, i.visibility, i.size.to_i / 1024 / 1024, i.min_ram, i.min_disk] }
+            headers << ['Visibility', 'Size (Mo)', 'Min RAM (Go)', 'Min Disk (Go)']
+            headers = headers.flatten
+          else
+            images = env[:openstack_client].nova.get_all_images(env)
+            images.each { |image| rows << [image.id, image.name] }
+          end
+          display_table(env, headers, rows)
         end
       end
     end
