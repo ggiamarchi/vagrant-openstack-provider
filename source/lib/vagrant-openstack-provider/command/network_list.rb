ec1308e7e980e10969ff842cfc5602820b3d95e0
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2014-11-10 01:51:57 +0100

    Fix wrong variable name

--- source/lib/vagrant-openstack-provider/command/network_list.rb
+++ source/lib/vagrant-openstack-provider/command/network_list.rb
@@ -13,13 +13,13 @@ module VagrantPlugins
         def cmd(name, argv, env)
           fail Errors::UnrecognizedArgForCommand, cmd: name, arg: argv[1] if argv.size > 1
           if argv.size == 0
-            flavors = env[:openstack_client].neutron.get_private_networks(env)
+            networks = env[:openstack_client].neutron.get_private_networks(env)
           elsif argv[0] == 'all'
-            flavors = env[:openstack_client].neutron.get_all_networks(env)
+            networks = env[:openstack_client].neutron.get_all_networks(env)
           else
             fail Errors::UnrecognizedArgForCommand, cmd: name, arg: argv[0]
           end
-          display_item_list(env, flavors)
+          display_item_list(env, networks)
         end
       end
     end
