7b58060d773c53fa7ff015ed91ae09b84970d42e
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2015-02-17 01:23:31 +0100

    Fix loader errors

--- source/lib/vagrant-openstack-provider/action.rb
+++ source/lib/vagrant-openstack-provider/action.rb
@@ -1,9 +1,6 @@
 require 'pathname'
 
 require 'vagrant/action/builder'
-require 'vagrant-openstack-provider/version_checker'
-
-VagrantPlugins::Openstack.check_version
 
 module VagrantPlugins
   module Openstack
