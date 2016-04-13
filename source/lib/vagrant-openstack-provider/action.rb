699e89b232a378f69b3df7d375866c21f2a94f50
Charlie Sharpsteen <source@sharpsteen.net>
2016-04-13 05:56:13 -0700

    Switch to WaitForCommunicator
    The OpenStack provider's action_up used a custom WaitForServerToBeAccessible
    action to poll instances via SSH in order to determine when the VM is up and
    running. One large shortcoming of this custom implementation is that it does
    not support communicators other than SSH, notably WinRM. This patch replaces
    the WaitForServerToBeAccessible with the built-in WaitForCommunicator action
    which has been part of Vagrant since v1.3 and is able to handle multiple
    communicators.
    
    Fixes ggiamarchi/vagrant-openstack-provider#264.
    Closes ggiamarchi/vagrant-openstack-provider#227.

--- source/lib/vagrant-openstack-provider/action.rb
+++ source/lib/vagrant-openstack-provider/action.rb
@@ -105,7 +105,7 @@ module VagrantPlugins
               b2.use CreateStack
               b2.use CreateServer
               b2.use Message, I18n.t('vagrant_openstack.ssh_disabled_provisioning') if ssh_disabled
-              b2.use WaitForServerToBeAccessible unless ssh_disabled
+              b2.use WaitForCommunicator unless ssh_disabled
             when :shutoff
               b2.use StartServer
             when :suspended
@@ -216,7 +216,6 @@ module VagrantPlugins
       autoload :ProvisionWrapper, action_root.join('provision')
       autoload :WaitForServerToStop, action_root.join('wait_stop')
       autoload :WaitForServerToBeActive, action_root.join('wait_active')
-      autoload :WaitForServerToBeAccessible, action_root.join('wait_accessible')
 
       private
 
