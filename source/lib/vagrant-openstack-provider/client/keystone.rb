4b7a9d9007a2836f0ce4d404b7f1657b4c3779f1
Dale Smith <dale@catalyst.net.nz>
2019-06-28 09:57:13 +0100

    Remove unused function arg, and trailing comma from hash.

--- source/lib/vagrant-openstack-provider/client/keystone.rb
+++ source/lib/vagrant-openstack-provider/client/keystone.rb
@@ -25,7 +25,7 @@ module VagrantPlugins
         elsif config.identity_api_version == '3'
           if config.auth_type == 'token'
             @logger.info('Using token credentials.')
-            post_body = get_body_3_token config
+            post_body = get_body_3_token
           else
             post_body = get_body_3 config
           end
@@ -121,14 +121,14 @@ module VagrantPlugins
         }
       end
 
-      def get_body_3_token(config)
+      def get_body_3_token
         {
           auth:
           {
             identity: {
               methods: ['token'],
               token: {
-                id: '****',
+                id: '****'
               }
             }
           }
