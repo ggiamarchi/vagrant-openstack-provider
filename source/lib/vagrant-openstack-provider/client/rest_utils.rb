364e61991cbee40e8216afe9758041926963cf43
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2022-11-23 15:56:45 +0100

    Follow HTTP redirection
    Fixes #393
    Fixes #171

--- source/lib/vagrant-openstack-provider/client/rest_utils.rb
+++ source/lib/vagrant-openstack-provider/client/rest_utils.rb
@@ -11,28 +11,46 @@ module VagrantPlugins
         end
       end
 
-      def self.get(env, url, headers = {}, &block)
+      def self.get(env, url, headers = {})
         config = env[:machine].provider_config
         _set_proxy(config)
-        RestClient::Request.execute(method: :get, url: url, headers: headers,
-                                    timeout: config.http.read_timeout, open_timeout: config.http.open_timeout,
-                                    ssl_ca_file: config.ssl_ca_file, verify_ssl: config.ssl_verify_peer, &block)
+        begin
+          resp = RestClient::Request.execute(method: :get, url: url, headers: headers,
+                                             timeout: config.http.read_timeout, open_timeout: config.http.open_timeout,
+                                             ssl_ca_file: config.ssl_ca_file, verify_ssl: config.ssl_verify_peer)
+        rescue RestClient::ExceptionWithResponse => err
+          resp = err.response
+        end
+
+        resp
       end
 
-      def self.post(env, url, payload, headers = {}, &block)
+      def self.post(env, url, payload, headers = {})
         config = env[:machine].provider_config
         _set_proxy(config)
-        RestClient::Request.execute(method: :post, url: url, payload: payload, headers: headers,
-                                    timeout: config.http.read_timeout, open_timeout: config.http.open_timeout,
-                                    ssl_ca_file: config.ssl_ca_file, verify_ssl: config.ssl_verify_peer, &block)
+        begin
+          resp = RestClient::Request.execute(method: :post, url: url, payload: payload, headers: headers,
+                                             timeout: config.http.read_timeout, open_timeout: config.http.open_timeout,
+                                             ssl_ca_file: config.ssl_ca_file, verify_ssl: config.ssl_verify_peer)
+        rescue RestClient::ExceptionWithResponse => err
+          resp = err.response
+        end
+
+        resp
       end
 
-      def self.delete(env, url, headers = {}, &block)
+      def self.delete(env, url, headers = {})
         config = env[:machine].provider_config
         _set_proxy(config)
-        RestClient::Request.execute(method: :delete, url: url, headers: headers,
-                                    timeout: config.http.read_timeout, open_timeout: config.http.open_timeout,
-                                    ssl_ca_file: config.ssl_ca_file, verify_ssl: config.ssl_verify_peer, &block)
+        begin
+          resp = RestClient::Request.execute(method: :delete, url: url, headers: headers,
+                                             timeout: config.http.read_timeout, open_timeout: config.http.open_timeout,
+                                             ssl_ca_file: config.ssl_ca_file, verify_ssl: config.ssl_verify_peer)
+        rescue RestClient::ExceptionWithResponse => err
+          resp = err.response
+        end
+
+        resp
       end
     end
   end
